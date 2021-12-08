585ddaae7a8f19a794476f76bc1851103bf64cb3
makobako <75222401+makobako@users.noreply.github.com>
2021-12-08 21:15:36 +0000

    modified Lsystem tree

--- turtle/Lsystem_tree.py
+++ turtle/Lsystem_tree.py
@@ -1,8 +1,5 @@
 import random
 import turtle
-import math
-
-RADIANS = 180 / math.pi
 
 wn = turtle.Screen()
 wn.bgcolor("black")
@@ -14,36 +11,6 @@ tim = turtle.Turtle()
 tim.color('blue', 'purple')
 tim.begin_fill()
 tim.speed(0)
-
-
-def find_opposite(distance_from_center, number_of_branches, length_of_the_adjacent):
-    adjacent = length_of_the_adjacent
-    number_of_gaps = number_of_branches + 1
-    angle = 180 / number_of_gaps
-    this_branches_angle = angle * distance_from_center
-    radians_this_branches_angle = math.radians(this_branches_angle)
-
-    opposite = math.tan(radians_this_branches_angle) * adjacent
-
-    return opposite
-
-
-def find_hypotenuse(distance_from_center, number_of_branches, length_of_the_adjacent):
-    opposite = find_opposite(distance_from_center, number_of_branches, length_of_the_adjacent)
-    number_of_gaps = number_of_branches + 1
-    angle = 180 / number_of_gaps
-    this_branches_angle = angle * distance_from_center
-    radians_this_branches_angle = math.radians(this_branches_angle)
-
-    hypotenuse = opposite / math.sin(radians_this_branches_angle)
-
-    return hypotenuse
-
-
-def tim_fb(distance):
-    tim.forward(distance)
-    tim.rotate(180)
-    tim.forward(distance)
 
 
 def my_tree(tree, recursion_number):
@@ -150,22 +117,6 @@ def get_turtle_state(_turtle):
 def restore_turtle_state(_turtle, state):
     _turtle.setheading(state[0])
     _turtle.setposition(state[1][0], state[1][1])
-
-
-print(find_opposite(1, 3, 100))
-
-print(find_hypotenuse(1, 3, 100))
-
-# num_of_stages = int(input("number of stages"))
-# branch_numbers = []
-# tree = []
-
-# for i in range(num_of_stages):
-
-#    branch_numbers.append(int(input("number of branches")))
-
-
-# tim.forward(100)
 
 
 tim.left(90)
