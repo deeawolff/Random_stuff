5c71fbc8a308bbfe26c9a78e8e46d3f807e10b79
makobako <75222401+makobako@users.noreply.github.com>
2021-12-12 21:09:44 +0000

    done stuff

--- processing/cells_and_food.py
+++ processing/cells_and_food.py
@@ -31,6 +31,12 @@ def find_a_from_hyoptenuse_angel(hypotenuse, angle):
 def find_difference_between_two_coordinates(p1, p2):
     return [p1[0] - p2[0], p1[1] - p2[1]]
 
+def are_objects_touching_each_other(p1, p2, p1size, p2size):
+    if p1[0] + p1size > p2[0] - p2size:
+        if p1[0] - p1size < p2[0] + p2size:
+            if p1[1] + p1size > p2[1] - p2size:
+                if p1[1] - p1size < p2[1] + p2size:
+                    return True
 
 class Cell:
     def __init__(self, number, food, position, colour):
@@ -40,26 +46,29 @@ class Cell:
         self.colour = colour
         self.this_cells_food = foods[random.randint(0, 2)]
         self.food_position = self.this_cells_food.return_position()
-        self.food = food
+        self.size = food
         self.ready_for_deletion = False
         self.eating = False
+        self.cells_touching = []
 
     def update(self):
 
         if not self.eating:
-            self.food -= 0.02
+            self.size -= 0.02
 
         self.move_towards_food()
         self.try_and_eat()
         self.try_and_divide()
         self.see_if_dead()
+        # if self.touching_other_cell():
+        #     self.try_share_food()
 
         if self.this_cells_food.size <= 1:
             self.eating = False
             self.find_new_food()
 
         app.fill(self.colour[0], self.colour[1], self.colour[2])
-        app.ellipse(self.position[0], self.position[1], self.food, self.food)
+        app.ellipse(self.position[0], self.position[1], self.size, self.size)
 
     def move_towards_food(self):
         difference_between_coordinates = find_difference_between_two_coordinates(self.position, self.food_position)
@@ -77,31 +86,40 @@ class Cell:
         return
 
     def try_and_eat(self):
-        if self.position[0] > self.food_position[0] - self.this_cells_food.size:
-            if self.position[0] < self.food_position[0] + self.this_cells_food.size:
-                if self.position[1] > self.food_position[1] - self.this_cells_food.size:
-                    if self.position[1] < self.food_position[1] + self.this_cells_food.size:
-                        self.this_cells_food.get_eaten()
-                        self.food += 0.2
-                        self.eating = True
+        if are_objects_touching_each_other(self.position, self.food_position, self.size, self.this_cells_food.size):
+            self.this_cells_food.get_eaten()
+            self.size += 0.2
+            self.eating = True
 
     def find_new_food(self):
         self.this_cells_food = foods[random.randint(0, len(foods) - 1)]
         self.food_position = self.this_cells_food.return_position()
 
     def try_and_divide(self):
-        if self.food >= 10:
+        if self.size >= 10:
             if not random.randint(0, 10):
                 cells.append(
                     Cell(i, 5, self.position, [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]))
             else:
                 cells.append(Cell(i, 5, self.position, self.colour))
-            self.food -= 5
+            self.size -= 5
 
     def see_if_dead(self):
-        if self.food <= 1:
+        if self.size <= 1:
             self.ready_for_deletion = True
 
+    def touching_other_cell(self):
+        for x in range(len(cells)):
+            if are_objects_touching_each_other(self.position, cells[x].position, self.size,
+                                               cells[x].this_cells_food.size):
+                self.cells_touching.append(cells[i])
+                return True
+        self.cells_touching = []
+        return False
+
+    # def try_share_food(self):
+    #     for x in range(len(self.cells_touching)):
+    #         if
 
 class Food:
     def __init__(self, number):
@@ -134,9 +152,7 @@ for i in range(10):
     cells.append(Cell(i, 5, [random.randint(1, screen_size[0]), random.randint(1, screen_size[1])],
                       [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]))
 
-print("test")
-
-while (True):
+while True:
     app.background(0, 0, 0)  # set background:  red, green, blue
     app.fill(255, 255, 0)  # set color for objects: red, green, blue
     cells_deleted_this_time_round = 0
