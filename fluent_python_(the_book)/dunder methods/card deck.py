58161c5ce006f5ee47e241f655d92bd5a3c08c0c
makobako <75222401+makobako@users.noreply.github.com>
2021-12-18 15:28:04 +0000

    done stuff

--- fluent_python_(the_book)/dunder methods/card deck.py
+++ fluent_python_(the_book)/dunder methods/card deck.py
@@ -2,11 +2,17 @@ import collections
 from random import choice
 
 Card = collections.namedtuple('Card', ['rank', 'suit'])
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+
+def spades_high(card):
+    rank_value = Deck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
 
 
 class Deck:
     ranks = [str(n) for n in range(2, 11)] + list('JQKA')
-    suits = 'spades clubs dimonds hearts'.split()
+    suits = 'spades clubs diamonds hearts'.split()
 
     def __init__(self):
         self._cards = [Card(rank, suit) for suit in self.suits
@@ -18,13 +24,29 @@ class Deck:
     def __getitem__(self, position):
         return self._cards[position]
 
+    def __setitem__(self, key, value):
+        self._cards[key] = value
+
 
 deck = Deck()
 
 print(len(deck))
+print("\n")
 
 print(deck[3])
+print("\n")
 
 print(choice(deck))
-for card in reversed(deck):
+print("\n")
+
+for card in sorted(deck, key=spades_high):
     print(card)
+
+print("\n")
+print(deck[3])
+
+print("\n")
+deck[3].rank = 5
+
+print("\n")
+print(deck[3])
