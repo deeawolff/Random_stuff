89be22eee0fefa8493174439650b951090f7666b
makobako <75222401+makobako@users.noreply.github.com>
2021-12-18 13:49:56 +0000

    made a card deck

---
+++ fluent_python_(the_book)/dunder methods/card deck.py
@@ -0,0 +1,30 @@
+import collections
+from random import choice
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+
+class Deck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades clubs dimonds hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits
+                       for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+
+deck = Deck()
+
+print(len(deck))
+
+print(deck[3])
+
+print(choice(deck))
+for card in reversed(deck):
+    print(card)
