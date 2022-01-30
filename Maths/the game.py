a567746251f3a60529daa9f92b66d9a0d0b99476
makobako <75222401+makobako@users.noreply.github.com>
2022-01-30 11:59:09 +0000

    got it to calculate how many square numbers it needs (it goes a bit high i think but it works

--- Maths/the game.py
+++ Maths/the game.py
@@ -2,16 +2,21 @@ import math
 
 from processing_py import *
 
-screen_size = (600, 400)
+screen_size = (1200, 800)
 app = App(screen_size[0], screen_size[1])  # create window: width, height
 
-answers = [[] for i in range(100)]
+answers = [[] for i in range(23)]
 
 angle = 360 / len(answers)
-print(f"angle = {angle} len(answers) = {len(answers)}  angle * len(answers) = {angle * len(answers)}")
+# print(f"angle = {angle} len(answers) = {len(answers)}  angle * len(answers) = {angle * len(answers)}")
+n2plus1 = [2 * n + 1 for n in range(len(answers))]
 
-squares = [i * i for i in range(len(answers))]
-print(squares)
+n2plus1_counter = 0
+while n2plus1[n2plus1_counter] < len(answers):
+    n2plus1_counter += 1
+
+print(n2plus1_counter)
+squares = [i * i for i in range(0, n2plus1_counter)]
 
 for i in range(1, len(answers) + 1):
     for x in range(len(squares)):
@@ -36,7 +41,7 @@ class pointOnTheCircle:
         self.size = circumference / number_of_points_on_circle
 
 
-points_on_the_circle = [pointOnTheCircle(i, angle, screen_size, 100) for i in range(len(answers))]
+points_on_the_circle = [pointOnTheCircle(i, angle, screen_size, screen_size[1] / 3) for i in range(len(answers))]
 while (True):
     app.textSize(22)
     app.background(255, 255, 255)  # set background:  red, green, blue
